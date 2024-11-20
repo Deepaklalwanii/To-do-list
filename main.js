f1c980653fad0e31946296ef3ded4b4db1b81089
deepaklalwanii <deepaklalwani752@gmail.com>
2024-11-21 01:01:45 +0530

    html-changes

--- main.js
+++ main.js
@@ -71,8 +71,7 @@ function addTaskToList(list, taskContent, listKey) {
     });
 }
 
-
-
+git 
 document.querySelector('#to-do').addEventListener('click', function() {
     let taskContent = input.value.trim();
     if (taskContent !== '') {
